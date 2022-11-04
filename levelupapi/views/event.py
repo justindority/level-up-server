7ad880551e5cea5308331c5cf2e53cf8259ce5d9
Justin Dority <justindority@gmail.com>
2022-11-04 16:10:19 -0500

    working on PUTs

--- levelupapi/views/event.py
+++ levelupapi/views/event.py
@@ -3,7 +3,8 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
-from levelupapi.models import Event
+from levelupapi.models import Event, Game, Gamer
+from datetime import date
 
 
 class EventView(ViewSet):
@@ -31,10 +32,49 @@ class EventView(ViewSet):
         serializer = EventSerializer(events, many=True) 
         return Response(serializer.data)
 
+    def create(self, request):
+        """Handle POST operations
+
+        Returns
+            Response -- JSON serialized event instance
+        """
+        gamer = Gamer.objects.get(user=request.auth.user)
+        game = Game.objects.get(pk=request.data["game"])
+
+        event = Event.objects.create(
+            game=game,
+            description=request.data["description"],
+            date=request.data["date"],
+            time=request.data["time"],
+            organizer=gamer
+        )
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+
+    def update(self, request, pk):
+        """Handle PUT requests for an event
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        event = Event.objects.get(pk=pk)
+        event.title = request.data["title"]
+        event.maker = request.data["maker"]
+        event.number_of_players = request.data["number_of_players"]
+        event.skill_level = request.data["skill_level"]
+
+        game_type = GameType.objects.get(pk=request.data["game_type"])
+        event.game_type = game_type
+        event.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
+
+
 
 class EventSerializer(serializers.ModelSerializer):
     """JSON serializer for events
     """
     class Meta:
         model = Event
-        fields = ('id', 'game', 'description', 'date', 'time', 'organizer', 'gamers')
+        fields = ('id', 'game', 'description', 'date', 'time', 'organizer')
